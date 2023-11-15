7845b70eda84001459aa58022894fac328558cfa
joaouzeloto <joaoricardouzelotosilva2003@gmail.com>
2023-11-15 20:44:49 -0300

    - new views

--- PedidosFX/src/main/java/br/fipp/pedidosfx/CategoriaCadViewController.java
+++ PedidosFX/src/main/java/br/fipp/pedidosfx/CategoriaCadViewController.java
@@ -24,7 +24,8 @@ public class CategoriaCadViewController extends Application {
     }
 
     public void onConfirmar(ActionEvent actionEvent) {
-        Categoria cat1 = new Categoria(tfCategoria.toString(),tfDescricao.toString());
+        Categoria cat1 = new Categoria(tfCategoria.getText(),tfDescricao.getText());
+        System.out.println(cat1.getNome() + " " +cat1.getDescricao());
         CategoriaDAL cat2 = new CategoriaDAL();
         cat2.gravar(cat1);
         ((Button)actionEvent.getSource()).getScene().getWindow().hide();
