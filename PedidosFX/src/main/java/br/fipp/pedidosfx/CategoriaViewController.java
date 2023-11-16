fb0f5d230e0917acca3d872b1984233754fccb6b
Diego Godoy <85560773+diegogodoy06@users.noreply.github.com>
2023-11-16 00:07:26 -0300

    view table in start

--- PedidosFX/src/main/java/br/fipp/pedidosfx/CategoriaViewController.java
+++ PedidosFX/src/main/java/br/fipp/pedidosfx/CategoriaViewController.java
@@ -1,18 +1,37 @@
 package br.fipp.pedidosfx;
 
+import br.fipp.pedidosfx.db.DBSingleton;
+import br.fipp.pedidosfx.db.entidades.Categoria;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import br.fipp.pedidosfx.db.entidades.Cliente;
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class CategoriaViewController extends Application {
+
+
+    @FXML
+    private TableView<Categoria> tableView;
+    @FXML
+    private TableColumn<Categoria, Integer> colID;
+    @FXML
+    private TableColumn<Categoria, String> colNome;
+    @FXML
+    private TableColumn<Categoria, String> colDescricao;
+
 
     public static void main(String[] args) {
         launch(args);
@@ -20,6 +39,29 @@ public class CategoriaViewController extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("categoria-view.fxml"));
+            Object root = fxmlLoader.load();
+            CategoriaViewController controller = fxmlLoader.getController();
+
+            Scene scene = null;
+            try {
+                scene = new Scene(fxmlLoader.load());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            primaryStage.setTitle("Sua Aplicação");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+            // Carregar dados do banco de dados
+            //controller.carregarDadosDoBanco();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
